6c3ed02b4f378d853c65ba62753f12acc99e3334
Mark D. Thompson <thomesoni@gmail.com>
2018-11-27 21:59:46 -0500

    added rinkeby network

--- truffle.js
+++ truffle.js
@@ -14,6 +14,7 @@
 
 var mnemonic = "wild column boat sting review caught vocal spice option switch raccoon swap";
 var infura = "https://rinkeby.infura.io/v3/9e9386f54cdf430f885c6ededa4e4475";
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
  module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -23,6 +24,11 @@ var infura = "https://rinkeby.infura.io/v3/9e9386f54cdf430f885c6ededa4e4475";
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, infura),
+      network_id: "4",
+      gas: 4500000
     }
   }
 };
